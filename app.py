53c6a5b2cf5d002e205a4e90de61629f0abe8a6f
jaimekruger <jaime@bitworthy.io>
2023-06-14 16:57:13 +0200

    Functional and aesthetic improvements

--- app.py
+++ app.py
@@ -30,10 +30,12 @@ JOBS = [
 @app.route("/")
 def hello_world():
     return render_template("home.html", jobs=JOBS)
-
+'''
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+'''
+
 
 if __name__ == "__main__":
     app.run(debug=True)
